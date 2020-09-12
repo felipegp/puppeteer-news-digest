b3c7d2a5b9431a32273cc006e46695adbef3a66d
FELIPE GOMES DO PRADO <felipegomes.cit@sulamerica.com.br>
2020-09-11 22:06:40 -0300

    adding prompt input

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import puppeteer from 'puppeteer';
+import prompt from 'prompt';
 import { NEWS } from './src/domain/news.js';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const labels = await getNews(NEWS.HACKERNOON, page);
+  const choices = NEWS.reduce((acc, val, index) => {
+    return acc + ` [${index}] ${val.title}        `;
+  }, '');
 
+  const choice = await new Promise((resolve) => {
+    prompt.get([choices], (error, result) => {
+      resolve(result[choices]);
+    });
+  });
+
+  const labels = await getNews(NEWS[choice], page);
   console.log(labels);
 
   await browser.close();
--- src/domain/news.js
+++ src/domain/news.js
@@ -1,14 +1,17 @@
-export const NEWS = {
-  UOL: {
+export const NEWS = [
+  {
+    title: 'Uol',
     url: 'https://uol.com.br',
     selector: '.manchete-editorial a, .mais-lidas-container a',
   },
-  G1: {
+  {
+    title: 'G1',
     url: 'https://g1.globo.com/',
     selector: '.bstn-hls a, .post-mais-lidas a',
   },
-  HACKERNOON: {
+  {
+    title: 'Hackernoon',
     url: 'https://hackernoon.com/tagged/hackernoon-top-story/',
     selector: '.title-wrapper a',
   },
-};
+];
